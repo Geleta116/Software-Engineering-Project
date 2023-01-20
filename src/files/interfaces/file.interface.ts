18ed778b04fd9a461084f7d34ec6d460306388cf
Mathias Wakgari <104831041+mathiaswakgari@users.noreply.github.com>
2023-01-20 20:11:51 +0300

    Create file.interface.ts

---
+++ src/files/interfaces/file.interface.ts
@@ -0,0 +1,10 @@
+export interface File{
+    fileName : String;
+    description : String;
+    department : String;
+    course : String;
+    year : Number;
+    like : Number;
+    filePath: String;
+
+}
