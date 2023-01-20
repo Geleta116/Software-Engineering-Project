ed89bb3d67487fd80bd50d98bbf3bc90c0d70238
Mathias Wakgari <104831041+mathiaswakgari@users.noreply.github.com>
2023-01-20 20:17:25 +0300

    Create videoService.js

---
+++ src/services/videoService.js
@@ -0,0 +1,9 @@
+const videos = [
+  "https://www.youtube.com/embed/e74FaoCkSjM",
+  "https://www.youtube.com/embed/cnNMo4u0L4M",
+  "https://www.youtube.com/embed/7ZEYQ30iBTc",
+];
+
+export function getVideos() {
+  return videos;
+}
